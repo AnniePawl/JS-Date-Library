9bbe8b7c31d2a2e1ad58d0d088077d4ff2ede3fb
Anna Pawl <annampawl@gmail.com>
2019-12-05 16:18:31 -0800

    reformatted index and tests

--- src/index.js
+++ src/index.js
@@ -1,78 +1,117 @@
-class Date {
+class Dates {
   constructor(...args) {
     this.date = new Date(...args);
-    // this.monthMap = {
-    //   0: 'January',
-    //   2: 'February',
-    //   3: 'March',
-    //   4: 'April',
-    //   5: 'May',
-    //   6: 'June',
-    //   7: 'July',
-    //   8: 'August',
-    //   9: 'September',
-    //   10: 'November',
-    //   11: 'December',
-    // }
   }
+
+  // year()
+  // Returns full year (4 digits)
+  fullYear() {
+    return this.date.getFullYear();
+  }
+
+  // shortYear()
+  // Returns short-hand year (2 digits)
+  shortYear() {
+    return this.date.getYear();
+  }
+
+  // monthName()
+  // Returns month
+  monthName() {
+    const m = this.date.getMonth();
+    const months = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December'
+    ]
+    return months[m]
+  }
+
+  // dayName()
+  // Returns day of week
+  dayName() {
+    const d = this.date.getDay()
+    const days = [
+      'Sunday',
+      'Monday',
+      'Tuesdays',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday'
+    ]
+    return days[d]
+  }
+
+  // hrs()
+  // Returns hours 
+  hr() {
+    return this.date.getHours()
+  }
+
+  // min()
+  // Returns minutes 
+  min() {
+    return this.date.getMinutes();
+  }
+
+  // sec()
+  // Returns seconds 
+  sec() {
+    return this.date.getMinutes()
+  }
+
+  // ms() 
+  // Returns milliseconds
+  ms() {
+    return this.date.getMilliseconds()
+  }
+
 }
 
 
-// Instantiate class 
-const d = new Date('9/26/1965')
-
-// year()
-// Returns full year (4 digits)
-Date.prototype.year = function () {
-  return d.getFullYear(this);
-}
-
-// shortYear()
-// Returns short-hand year (2 digits)
-Date.prototype.shortYear = function () {
-  return d.getYear(this);
-}
-
-// month()
-// Returns month
-Date.prototype.month = function () {
-  return d.getMonth(this);
-}
-
-// date()
-// Returns date
-Date.prototype.date = function () {
-  return d.getDate();
-}
-
-// day()
-// Returns day of week
-Date.prototype.day = function () {
-  return d.getDay(this);
-}
-
-// hrs()
-// Returns hours 
-Date.prototype.hrs = function () {
-  return d.getHours(this);
-}
-
-// min()
-// Returns minutes 
-Date.prototype.min = function () {
-  return d.getMinutes(this);
-}
-
-// sec()
-// Returns seconds 
-Date.prototype.sec = function () {
-  return d.getSeconds(this);
-}
-
-// ms() 
-// Returns milliseconds
-Date.prototype.ms = function () {
-  return d.getMilliseconds(this)
-}
+// Instantiate 
+const d = new Dates('4/11/1993')
+console.log(d.fullYear())
+console.log(d.shortYear())
+console.log(d.monthName())
+console.log(d.dayName())
+console.log(d.hr())
+console.log(d.min())
+console.log(d.sec())
+console.log(d.ms())
 
 
+
+
+// // date()
+// // Returns date of month (number)
+// date() {
+//   return this.date.getDate();
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+// mask = {
+
+// }
+
+module.exports = Dates;
--- tests/test.js
+++ tests/test.js
@@ -1,37 +1,51 @@
-require('../src/index')
+const Dates = require('../src/index')
+const d = new Dates('04/11/1993')
 
-// year() Test 
+// fullYear() Test 
 // Returns full year (4 digits)
 test('year', () => {
-  expect(d.year()).toBe()
+  expect(d.fullYear()).toBe(1993)
 })
 
 // yearShort() Test
 // Returns short year (2 digits)
-test('yearShort', () => {
-  expect(d.yearShort()).toBe()
+test('shortYear', () => {
+  expect(d.shortYear()).toBe(93)
 })
 
-// month() Test 
-// Returns month 
-test('month', () => {
-  expect(d.month()).toBe()
+// monthName() Test 
+// Returns the name of the month
+test('monthName', () => {
+  expect(d.monthName()).toBe('April')
 })
 
-// date() Test
-// Returns date
-
-// day() Test 
+// dayName() Test 
 // Returns day of the week 
+test('dayName', () => {
+  expect(d.dayName()).toBe('Sunday')
+})
 
-// hrs() Test 
+// hr() Test 
 // Returns hours
+test('hr', () => {
+  expect(d.hr()).toBe(0)
+})
 
 // min() Test 
 // Returns minutes
+test('min', () => {
+  expect(d.min()).toBe(0)
+})
 
 // sec() Test
 // Returns seconds
+test('sec', () => {
+  expect(d.sec()).toBe(0)
+})
 
 // ms() Test
 // Returns milliseconds
+test('ms', () => {
+  expect(d.ms()).toBe(0)
+})
+
